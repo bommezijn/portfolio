fd7c148b72788248b3c967294bec55cbd8e4d183
dewarian <nathanbommezijn@gmail.com>
2021-12-07 15:14:33 +0100

    :tada: begin new portfolio

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <body className='font-mono flex flex-col items-center justify-center min-h-screen'>
+      <main>
+        <h1 className='font-bold'>Hello World</h1>
+      </main>
+    </body>
+  );
+}
