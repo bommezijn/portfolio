477087128cbc44467684a12ada806a6d9a491271
dewarian <nathanbommezijn@gmail.com>
2021-12-09 22:21:55 +0100

    :construction: Start of portfolio

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
+import react from "react";
+import { FiInstagram, FiAperture, FiInfo } from "react-icons/fi";
+
 export default function Home() {
   return (
-    <body className='font-mono flex flex-col items-center justify-center min-h-screen'>
-      <main>
-        <h1 className='font-bold'>Hello World</h1>
-      </main>
-    </body>
+    <main className='font-mono flex flex-col items-center justify-center min-h-screen'>
+      <nav>
+        <ul>
+          <NavItem name='test' icon={FiInstagram} />
+        </ul>
+      </nav>
+    </main>
   );
 }
+
+const NavItem = ({ name, icon }) => {
+  return (
+    <>
+      <li>
+        <a href='' className='flex'>
+          {react.createElement(icon)}
+          <span className='ml-2'>{name}</span>
+        </a>
+      </li>
+    </>
+  );
+};
