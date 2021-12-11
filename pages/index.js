aaa045545f6e3526375a428ebfcfc128050438c8
dewarian <nathanbommezijn@gmail.com>
2021-12-11 10:49:24 +0100

    :poop: something in construction

--- pages/index.js
+++ pages/index.js
@@ -8,26 +8,28 @@ export default function Home() {
             return (
               <li
                 key={i}
-                className='first:text-[#FFCC00] first:font-bold first:text-2xl hover:first:text-red-400 capitalize text-l text-gray-400 transition delay-150 ease-in hover:text-black'>
+                className='first:text-[#FFCC00] first:font-bold first:text-2xl hover:first:text-red-400 text-l capitalize text-gray-400 transition delay-150 ease-in hover:text-black'>
                 {e}
               </li>
             );
           })}
         </ul>
       </nav>
-      <section className=''>
+      <section className='border-dashed border-2 border-red-900'>
         {/* <section className='flex flex-row justify-center items-center'> */}
-        <h1>Introduction</h1>
-        <section className='grid gap-8 grid-cols-2 border-dotted border-2 border-gray-200 min-w-full'>
-          <article className='bg-red-200 border-solid border-2 border-black p-4'>
-            <p>Hello Card</p>
-            <a href='#'>test link</a>
-          </article>
-          <article className='bg-red-200 border-solid border-2 border-black p-4'>
-            <p>Introduction bout me</p>
-            <a href='#'>test link</a>
-          </article>
-        </section>
+        <div className='l-0 m-auto max-w-7xl bg-yellow-400'>
+          {/* <h1>Introduction</h1> */}
+          <section className='grid gap-8 sm:grid-cols-1 md:grid-cols-2 border-dotted border-2 border-gray-400 py-2 min-w-full'>
+            <article className='bg-red-200 border-solid border-2 border-black p-4'>
+              <p>Hello Card</p>
+              <a href='#'>test link</a>
+            </article>
+            <article className='bg-red-200 border-solid border-2 border-black p-4'>
+              <p>Introduction bout me</p>
+              <a href='#'>test link</a>
+            </article>
+          </section>
+        </div>
         <h1>Projects</h1>
         <section className='grid gap-4 grid-cols-3'>
           <article className='bg-green-200 border-solid border-2 border-black p-4 transition ease-in hover:shadow-md'>
