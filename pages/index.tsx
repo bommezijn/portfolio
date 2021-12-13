b2e574fed9c2a7ff8e090030d194abb96e3bd784
dewarian <nathanbommezijn@gmail.com>
2021-12-13 17:16:12 +0100

    :recycle: Refactor and modularize

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ TODO: Convert className to {styles.CLASSNAME}
 */
 
 const Home: NextPage = () => {
-  return  (
+  return (
     <main className={styles.container}>
       <div id="content">
         <section className="grid">
@@ -19,25 +19,6 @@ const Home: NextPage = () => {
           {/* loop to add X amount of projects with title, desc. photo? */}
         </section>
       </div>
-      <footer>
-        <p>nethen bummezin</p>
-        <div className="social">
-          <ul>
-            <li>logo: social</li>
-            <li>logo: social</li>
-            <li>logo: social</li>
-            <li>logo: social</li>
-          </ul>
-        </div>
-        <form action="" method="post">
-          <p>If you wanna contact me tell me with an e-mail or via social media</p>
-          <label htmlFor=""><span>name</span><input type="text" name="" id="" required aria-required/></label>
-          <label htmlFor=""><input type="email" name="" id="" required /></label>m
-          <label htmlFor=""><span>whatcha wanna say</span><input type="textarea" name="" id="" required aria-required /></label>
-          <input type="button" value="Send me an e-mail" />
-        </form>
-        <p>Made with ♥ by <a href="https://github.com/bommezijn">Bommezijn</a></p>
-      </footer>
     </main>
   )
 }
