4ff72443eb22302c2d9a5adfcc680f0574767942
dewarian <nathanbommezijn@gmail.com>
2021-12-13 23:53:10 +0100

    :bulb: remove comments in home

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,6 @@ import Image from 'next/image'
 import Card from '../components/Card'
 import styles from '../styles/Home.module.css'
 
-/* 
-TODO: Convert className to {styles.CLASSNAME}
-*/
-
 const Home: NextPage = () => {
 
   return (
