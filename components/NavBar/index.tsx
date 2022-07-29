1420b43d0610164031d2c824e259fdf1cc78cc9f
dewarian <nathanbommezijn@gmail.com>
2022-07-29 12:21:53 +0200

    :poop: fix link to section

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -6,19 +6,19 @@ const NavBar: FunctionComponent = () => {
   return (
     <nav className={`${styles.navBar} ${styles.mildShadow}`}>
       <ul>
-        <Link href='/' passHref>
+        <Link href="/" passHref>
           <li>
             <a>Bommezijn</a>
           </li>
         </Link>
         <li className={styles.linkItem}>
-          <a href='#projects'>About</a>
+          <a href="#about">About</a>
         </li>
         <li className={styles.linkItem}>
-          <a href='#projects'>Projects</a>
+          <a href="#projects">Projects</a>
         </li>
         <li className={styles.linkItem}>
-          <a href='#footer'>Contact</a>
+          <a href="#footer">Contact</a>
         </li>
       </ul>
     </nav>
