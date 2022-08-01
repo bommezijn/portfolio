1edf6ef35bdb7556874eaf20ef5cc52e0bbd603f
Bommezijn <nathanbommezijn@gmail.com>
2022-08-01 22:11:30 +0200

    ♻️ add new method of contact

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -4,29 +4,37 @@ import styles from './style.module.css';
 
 const Footer: FunctionComponent = () => {
   return (
-    <footer className={styles.footer} id='footer'>
+    <footer className={styles.footer} id="footer">
       <div className={styles.social}>
-        <h3>Resume && Socials</h3>
+        <hgroup>
+          <h3>Resume && Socials</h3>
+          <p></p>
+        </hgroup>
         <ul>
           <li>
-            <Link href='https://read.cv/nb' passHref={true}>
+            <Link href="https://read.cv/nb" passHref={true}>
               <a>resume</a>
             </Link>
           </li>
           <li>
-            <Link href='https://github.com/bommezijn' passHref={true}>
+            <Link href="https://github.com/bommezijn" passHref={true}>
               <a>Github</a>
             </Link>
           </li>
           <li>
-            <Link href='https://www.linkedin.com/in/nbmbp/' passHref={true}>
+            <Link href="https://www.linkedin.com/in/nbmbp/" passHref={true}>
               <a>LinkedIn</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="mailto:nathanbommezijn@gmail.com" passHref={true}>
+              <a>email me</a>
             </Link>
           </li>
         </ul>
       </div>
       <p className={styles.outro}>
-        powered ⚡️ by <a href='https://github.com/bommezijn'>Nathan</a>
+        powered ⚡️ by <a href="https://github.com/bommezijn">Nathan</a>
       </p>
     </footer>
   );
