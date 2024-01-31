83cae76e91f169c63fef035db2ceb9cfb815c557
Nathan B <nathanbommezijn@gmail.com>
2024-01-31 16:51:05 +0100

    :warning: Ignoring build error warning

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  ignoreBuildErrors: true,
   images: {
     domains: [
       'user-images.githubusercontent.com',
